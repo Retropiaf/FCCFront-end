c27fee34cac98f8efbb01e212e25493645b7780b
Retropiaf (Christiane Sawadogo) <sawadogochristiane@gmail.com>
2018-02-25 14:20:07 -0800

    Create chapter service

--- my-app/src/app/chapter-detail/chapter-detail.component.ts
+++ my-app/src/app/chapter-detail/chapter-detail.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Chapter } from '../chapter';
 
 @Component({
   selector: 'app-chapter-detail',
@@ -6,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./chapter-detail.component.css']
 })
 export class ChapterDetailComponent implements OnInit {
-
+  @Input() chapter: Chapter;
   constructor() { }
 
   ngOnInit() {
---
+++ my-app/src/app/chapter.service.ts
@@ -0,0 +1,10 @@
+import { Injectable } from '@angular/core';
+import { Chapter } from './chapter';
+import { CHAPTERS } from './mock-chapters';
+
+@Injectable()
+export class ChapterService {
+
+  constructor() { }
+
+}
