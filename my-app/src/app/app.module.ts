21edbed8b23aabeb1be79c74dd8218e000f9faa7
Retropiaf (Christiane Sawadogo) <sawadogochristiane@gmail.com>
2018-02-25 12:36:11 -0800

    Add chapters component

--- my-app/src/app/app.module.ts
+++ my-app/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { ChaptersComponent } from './chapters/chapters.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ChaptersComponent
   ],
   imports: [
     BrowserModule
